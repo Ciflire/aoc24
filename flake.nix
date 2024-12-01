e7131e3cfed0506dd78a1580a0b85eab66d99df0
VESSE Léo <leovesse@gmail.com>
2024-12-01 23:08:45 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/master";
+    };
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      #       ↑ Swap it for your system if needed
+      #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # backend related
+          cargo
+          rustc
+          rust-analyzer
+          rustfmt
+          # helpers
+          glow
+        ];
+        shellHook = "";
+      };
+    };
+}
